3af624c60b2dac3dabc37a75c2397679e5d5da8a
Benoist <benpresso@gmail.com>
2017-01-10 16:57:59 +0100

    ...

--- 1-run_container.sh
+++ 1-run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker rm -f $(docker ps -a -q)
+docker rm -f $(docker ps -a -q) > /dev/null
 docker run -ti -p 80:80 -p 443:443 -v $(pwd):/root/nginxfiles --name webserver ubuntu_sp bash
